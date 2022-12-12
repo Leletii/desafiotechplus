d1c754147eb5669712c47b94bb16d693aee1bdb2
Leticia Agante <leticia.nos@gmail.com>
2022-12-11 22:37:02 -0300

    implemented plus button style; added styled engine

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,7 @@
 import { Button, Paper, TextField } from "@mui/material";
 import React, { useState } from "react";
+import { styled, createTheme, ThemeProvider } from '@mui/system';
+
 
 export default function From({  addTodo }){
 
@@ -10,10 +12,28 @@ export default function From({  addTodo }){
         const todoObj = { text: text, id: id};
         setId(id + 1);
         addTodo(todoObj);
+        document.getElementById("standard-basic").value = null;
     };
+
+    const customTheme = createTheme({
+        palette: {
+          primary: {
+            main: '#1976d2',
+            contrastText: 'white',
+          },
+        },
+      });
+
+    const Button = styled('div')(({ theme }) => ({
+        color: theme.palette.primary.contrastText,
+        backgroundColor: theme.palette.primary.main,
+        padding: theme.spacing(1),
+        borderRadius: theme.shape.borderRadius,
+      }));
 
     return (
         <Paper style={{ padding: "1em" }}>
+            <h1 className="title"> Lista de Tarefas </h1>
             <div style={{ display: "flex", justifyContent: "center"}}>
                 <TextField 
                 id="standard-basic" 
@@ -22,8 +42,10 @@ export default function From({  addTodo }){
                 onChange={(e) => setText(e.target.value)}
                 fullWidth
                 />
-                <Button variant="text" onClick={() => todoCreate(text)}> + </Button>
             </div>
+            <ThemeProvider theme={customTheme}>
+                    <Button variant="text" onClick={() => todoCreate(text)}> + </Button>
+            </ThemeProvider>
         </Paper>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,18 +15,18 @@ export default function Home() {
         var filtered = todos.filter((todo) => todo.id !== id);
         setTodos(filtered);
     }
-
+      
     return (
     <Container maxWidth="xs" style={{marginTop: "1em" }}>
-        <h1 className="title"> Lista de Tarefas </h1>
         <From addTodo={  addTodo }/>
-        <List sx={{ marginTop: "1em"}}>
+        <List href="#foo" sx={{ marginTop: "1em"}}>
             {todos.map((todo) => (
                 <div key={todo.id}style={{ marginTop: "1em" }}>
                     <TodoItem todo={todo} deleteTodo={deleteTodo}/>
                 </div>
             ))}
         </List>
-      </Container>
+    </Container>
     );
+
 }
